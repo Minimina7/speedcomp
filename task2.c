b2a252c24157dba7a8371ed8dc835b828f5f9df5
cenepi <raltamimi0001@stu.kau.edu.sa>
2024-11-05 00:10:24 -0800

    v2

--- task2.c
+++ task2.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-	int intXsize=10;
-	int intYsize=10;
-	char chrGrid[intXsize][intYsize];
-
-	int intRobotPositionX=rand;
-	int intRobotPositio;
-
-
-
-
-
-
-	for(int i=0;i<intXsize;i++){
-		for(int j=0;j<intYsize;j++){
-			chrGrid[i][j]='$';
-		}
-	}
 
 int main(){
+        int     intXsize=10;
+        int     intYsize=10;
+        char    chrGrid[intXsize][intYsize];
 
-	
+        int intRobotPositionX=rand()%(intXsize);
+        int intRobotPositionY=rand()%(intYsize); 
+
+
+
+
+
+
+        for(int i=0;i<intXsize;i++){
+                for(int j=0;j<intYsize;j++){
+                        chrGrid[i][j]='$';
+                }
+        }
+
+
+	printf("%d\n",intRobotPositionX);
+        printf("%d\n",intRobotPositionY);
+
+
+return 0;
+}
