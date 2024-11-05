cb42a81965496e0137b4151d85356d31ec58f47d
cenepi <raltamimi0001@stu.kau.edu.sa>
2024-11-05 00:12:13 -0800

    v2

--- task2.c
+++ task2.c
@@ -10,20 +10,16 @@ int main(){
         int intRobotPositionX=rand()%(intXsize);
         int intRobotPositionY=rand()%(intYsize); 
 
-
-
-
-
+	int intCleaned=0;
+	int intTrash=intXsize*intYsize;
 
         for(int i=0;i<intXsize;i++){
                 for(int j=0;j<intYsize;j++){
                         chrGrid[i][j]='$';
                 }
         }
+	while (intCleaned<0.95*int
 
-
-	printf("%d\n",intRobotPositionX);
-        printf("%d\n",intRobotPositionY);
 
 
 return 0;
