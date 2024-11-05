3c7b787d6f8159de240a525d5d26959da6abcdea
cenepi <raltamimi0001@stu.kau.edu.sa>
2024-11-04 23:51:19 -0800

    task 2

---
+++ task2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+	int intXsize=10;
+	int intYsize=10;
+	char chrGrid[intXsize][intYsize];
+
+	int intRobotPositionX=rand;
+	int intRobotPositio;
+
+
+
+
+
+
+	for(int i=0;i<intXsize;i++){
+		for(int j=0;j<intYsize;j++){
+			chrGrid[i][j]='$';
+		}
+	}
+
+int main(){
+
+	
